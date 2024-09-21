56fee6c4de61b6026fa99b2a8fc7cc674b79c62a
FernandoBarbosa <barbosalozanol@gmail.com>
2024-09-20 21:41:16 -0600

    Implementación del cruce uniforme y la seleccion por torneo

--- conversiones.py
+++ conversiones.py
@@ -1,8 +1,6 @@
 import math
 import random
 import re
-from turtledemo.paint import switchupdown
-
 
 # Validando que todos los individuos tengan la misma longitud
 def valida_longitud(lstBinaria, l):
@@ -60,10 +58,55 @@ def convercion_adaptado(valReal, fnAdaptacion, lstMinMax):
 
     return valAdap, adaptacion
 
+# Mutación Simple
+def mutacion_simple(lst_binaria, total_mutacion, l):
+    cont = 0
+
+    while cont != total_mutacion:
+        # Elegir un individuo aleatorio de la lista
+        indice_individuo = random.randint(0, (len(lst_binaria) - 1))
+        individuo = list(lst_binaria[indice_individuo])  # Convertir el individuo a lista para modificar
+
+        # Elegir un bit aleatorio
+        bit = random.randint(0, l - 1)
+
+        # Realizar la mutación
+        if individuo[bit] == '0':
+            individuo[bit] = '1'
+        else:
+            individuo[bit] = '0'
+
+        # Convertir el individuo de nuevo a string y actualizar la lista original
+        lst_binaria[indice_individuo] = "".join(individuo)
+
+        cont += 1
+
+    return lst_binaria
+
 # Selección ruleta (ordenar de mayor a menor)
 def seleccion_ruleta(valAdap):
     valAdap.sort(reverse=True)  # Ordenar la lista de mayor a menor
     return valAdap
+
+# Selección Torneo
+def seleccion_torneo(valAdap):
+
+    # Obteniendo el número de individuos participantes
+    n = random.randint(1, len(valAdap))
+
+    torneo = random.sample(valAdap, n)
+
+    for i in range(len(torneo)):
+
+        ind1, ind2 = random.sample(range(len(torneo)), 2)
+
+        if valAdap[ind1] > valAdap[ind2]:
+            torneo.remove(ind2)
+
+        else:
+            torneo.remove(ind1)
+
+    return torneo
 
 # Algoritmo para comparar y reemplazar en la lista
 def reemplazar_padres(hijo1, hijo2, padre1, padre2, lst_binaria):
@@ -133,6 +176,43 @@ def cruce_dosPuntos(lst_binaria, cruces, l):
 
     return lst_binaria
 
+# Cruce Uniforme
+def cruce_uniforme(lst_binaria, cruces, l):
+    cont = 0
+    mascara = [random.randint(0, 1) for _ in range(l)]
+
+    while cont != cruces:
+
+        padre1 = lst_binaria[random.randint(0, len(lst_binaria) - 1)]
+        padre2 = padre1
+
+        while padre2 == padre1:
+            padre2 = lst_binaria[random.randint(0, len(lst_binaria) - 1)]
+
+        hijo1 = []
+        hijo2 = []
+
+        for i in range(l):
+            if mascara[i] == 0:
+                hijo1.append(padre1[i])
+                hijo2.append(padre2[i])
+            else:
+                hijo1.append(padre2[i])
+                hijo2.append(padre1[i])
+
+        hijo1 = ''.join(map(str, hijo1))
+        hijo2 = ''.join(map(str, hijo2))
+        lst_binaria = reemplazar_padres(hijo1, hijo2, padre1, padre2, lst_binaria)
+
+        cont += 1
+
+    return lst_binaria
+
+def encontrar_mejores(lst_binaria, valor_adaptacion):
+    mejores = [elem for elem in lst_binaria if int(elem,2) >= valor_adaptacion]
+
+    return mejores
+
 def main():
     # Entrada de Individuos en formato binario separado por comas
     indBin = input('Ingrese individuos binarios separados por comas>>> ')
@@ -180,22 +260,39 @@ def main():
     ruleta = seleccion_ruleta(adaptados)
     print('\n' + '>>>>>Ruleta:', ruleta, '\n')
 
-    # Solicitamos al usuario el % de cruces que quiere tener en el algoritmo y especificamos que usaremos un float
-    porcentajeCruce =  float(input("Qué porcentaje de cruce quieres manejar"))
+    # Solicitamos al usuario el % de cruces que quiere tener en el algoritmo
+    porcentajeCruce =  int(input("Qué porcentaje de cruce quieres manejar"))
 
     # Calculamos con una regla de 3 el número de cruces que corresponde con el porcentaje establecido
     noCruces = round((porcentajeCruce * len(lstBinaria)) / 100)  # >>> (% de Cruce x total de individuos) / 100%
 
     # Pedimos que elijan un tipo de cruce
-    tipo_cruce = int(input("Qué tipo de cruce deseas hacer? 1 = Cruce de un punto | 2 = Cruce de dos puntos"))
+    tipo_cruce = int(input("Qué tipo de cruce deseas hacer? 1 = Cruce de Un Punto | 2 = Cruce de Dos Puntos | 3 = Cruce Uniforme" + "\n" + ">>>"))
+    switch_cruce = {1:cruce_unpunto, 2:cruce_dosPuntos, 3:cruce_uniforme}
 
-    switch = {1:cruce_unpunto, 2:cruce_dosPuntos}
+    # Pedimos que elijan un tipo de mutación
+    tipo_mutacion = int(input("Qué tipo de mutación deseas utilizar? 1 = Mutación Simple" + "\n" + ">>>"))
+    switch_mutacion = {1: mutacion_simple}
 
-    # Usar get() con una función por defecto si 'tipo_cruce' no existe
-    funcion_cruce = switch.get(tipo_cruce, lambda *args: "Opción no válida")
+    # Solicitamos al usuario el % de mutación que quiere tener en el algoritmo
+    porcentaje_mutacion = int(input("Qué porcentaje de mutación quieres manejar >>> "))
+
+    # Calculamos con una regla de 3 el número de individuos a mutar
+    num_mutaciones = round((porcentaje_mutacion * len(lstBinaria)) / 100)  # >>> (% de mutación x total de individuos) / 100%
+
+    # Usar get() con una función por defecto si 'tipo_cruce' no exist
+    funcion_cruce = switch_cruce.get(tipo_cruce, lambda *args: "Opción no válida")
 
     # Ejecutar la función seleccionada con los parámetros
-    print(funcion_cruce(lstBinaria, noCruces, l))
+    lstBinaria = funcion_cruce(lstBinaria, noCruces, l)
+
+    funcion_mutacion = switch_mutacion.get(tipo_mutacion, lambda *args: "Opción no válida")
+    lstBinaria = funcion_mutacion(lstBinaria, num_mutaciones, l)
+
+    total_adaptados = encontrar_mejores(lstBinaria, adaptacion)
+
+    print(f"Se encontraron {len(total_adaptados)} individuos adaptados")
+    print(total_adaptados)
 
 if __name__ == "__main__":
     main()
